49c52ba0c63d81cf95128a518bb47dd6c84d4a02
noamdoron <noam.doron@microfocus.com>
2017-12-26 17:39:25 +0200

    initial commit

---
+++ lib/app.ts
@@ -0,0 +1,4 @@
+export {SrfJsonLogger, LogSwitch} from "./srf-json-logger";
+export * from "./srf-reporter-types";
+export {SrfReporterTypeGuard} from "./srf-reporter-type-guard";
+export {SrfReporterFacade} from "./srf-reporter-facade";
